34bf95d2250c6630855268d438582c711d09f417
pdhxiye <1548453612@qq.com>
2021-03-22 12:59:06 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/pdh/git/HelloGit.java
@@ -0,0 +1,13 @@
+package com.pdh.git;
+
+/**
+ * @author pdh
+ * @create 2021-03-22-12:57
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("更新1");
+
+    }
+}
